e1dc108dbde17ec59db9c8e4d7b615e779388c2d
martincorona007 <martincorona007@gmail.com>
2019-06-09 13:31:16 -0500

    3.0

--- project/main.cpp
+++ project/main.cpp
@@ -3,21 +3,22 @@
 #include <set>
 #include <map>
 #include <stdlib.h>
-typedef std::set<std::string> intset;//definicion de tipo de dato,conjunto de eneteros
+typedef std::set<char> intset;//definicion de tipo de dato,conjunto de eneteros
 typedef std::map<int, intset>row;
 typedef std::map<int, row> table;
 table N;
 typedef struct grammar{
-    std::string generator;
-    std::string generator2;
-    std::string terminaloGenerator;
+    char generator;
+    char generator2;
+    char generator3;
+    char terminal;
 
 };
 
 bool CYK(grammar G[],char*);
 int length(char*);
 void print(intset &a);
-std::set<std::string> buildset(char r);
+std::set<char> buildset(char r);
 
 int main()
 {
@@ -25,22 +26,22 @@ int main()
     std::cout << "write the word ";
     std::cin>>W;
 
-    printf("length %i",length(W));
+    //printf("length %i",length(W));
 
     grammar G[9];
 
 
     G[0].generator='S';
     G[0].generator2='C';
-    G[0].terminaloGenerator='A';
+    G[0].generator3='A';
 
     G[1].generator='B';
     G[1].generator2='S';
-    G[1].terminaloGenerator='D';
+    G[1].generator3='D';
 
     G[2].generator='R';
     G[2].generator2='K';
-    G[2].terminaloGenerator='B';
+    G[2].generator3='B';
 
     G[3].generator='A';
     G[3].generator2='S';
@@ -48,38 +49,28 @@ int main()
     G[4].generator='D';
     G[4].generator2='R';
 
-    G[5].generator=83;
-    G[5].terminaloGenerator=120;
+    G[5].generator='C';
+    G[5].terminal='l';
 
-    G[6].generator='C';
-    G[6].terminaloGenerator=108;
+    G[6].generator='K';
+    G[6].terminal='c';
 
-    G[7].generator=75;
-    G[7].terminaloGenerator=99;
+    G[7].generator='S';
+    G[7].terminal='x';
 
-    G[8].generator=82;
-    G[8].terminaloGenerator=116;
+    G[8].generator='R';
+    G[8].terminal='t';
 
     CYK(G,W);
 /*
-    S 83
-    C 67
-    A 65
-    R 82
-    K 75
-    B 66
-    D 68
-    l 108
-    c 99
-    x 120
-    t 116
+    S 83 C 67    A 65   R 82    K 75    B 66    D 68    l 108   c 99     x 120 t 116
 
     */
 
 printf("table \n\n");
-	for (int i = 1;i < length(W);i++) {
+	for (int i = 1;i <=length(W);i++) {
 
-		for (int j = 1;j < length(W);j++) {
+		for (int j = 1;j <= length(W);j++) {
 			printf("\nt[%i,%i]", i, j);
 			print(N[i][j]);
 		}
@@ -89,21 +80,28 @@ printf("table \n\n");
 bool CYK(grammar G[],char *w){
     int n=0;//length
     int a=0;//run over the word
+    int i=1;
         n=length(w);
-        printf(" x ");
-        for(int i=1;i<n;i++){
+
+
+        while(i<=n){
+
+
                 for(int x=0;x<9;x++){//searh on the struct grammar
-                    if(G[x].terminaloGenerator==w[a]){
-                        printf("%c",G[x].generator);
+                    if(G[x].terminal==w[a]){
+                        //printf("%c",G[x].generator);
+                        printf("\nconstruye el conjunto  %i,%i",i,1);
                         N[i][1]=buildset(G[x].generator);
                         a++;//run over the word
-
+                        i++;
                     }
+
+
                 }
 
-            for(int j=2;j<n;j++){
 
-            }
+
+
         }
         printf("\n");
     /*
@@ -117,8 +115,8 @@ bool CYK(grammar G[],char *w){
 
 return 2;
 }
-std::set<std::string> buildset(std::string r){
-    std::set<std::string> aux;
+std::set<char> buildset(char r){
+    std::set<char> aux;
     aux.insert(r);
     printf("  conjunto generado: ");
     print(aux);
@@ -127,7 +125,7 @@ return aux;
 void print(intset &a){
     intset::iterator i=a.begin();
     while(i!=a.end()){
-        printf("%i ",*i);
+        printf("%c ",*i);
         i++;
     }
 }
